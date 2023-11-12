94bdd68b969fcbbb7f501fa01595c8eb8931161c
Charlie <charliebarger04@gmail.com>
2023-11-12 15:23:20 -0700

    add morgan

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,30 @@
 import express from 'express';
-import { requestLogger, unknownEndpoint } from './utils/middleware';
-import playerRouter from './controllers/players';
+const morgan = require('morgan');
 import cors from 'cors';
-import { getCronJob } from './cronJobs';
-import tradeRouter from './controllers/trade';
+import 'express-async-errors'; // Import this early for async error handling
 import './utils/config';
-import { deleteAndUpdatePlayers } from './utils/redis/updatePlayersTradeValues';
+import { unknownEndpoint, errorHandler } from './utils/middleware';
+import playerRouter from './controllers/players';
+import tradeRouter from './controllers/trade';
+import { getCronJob } from './cronJobs';
+import { deleteAndUpdatePlayers } from './utils/redis/playerOperations';
+
 const baseUrl = process.env.BASE_URL || '/api';
 
 const app = express();
 // updatePlayersTradeValues();
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+} else {
+  app.use(morgan('short'));
+}
+
 app.use(cors());
 app.use(express.json());
-app.use(requestLogger);
 app.use(`${baseUrl}/trades`, tradeRouter);
 app.use(`${baseUrl}/players`, playerRouter);
+app.use(errorHandler);
 app.use(unknownEndpoint);
 
 //start cron job
